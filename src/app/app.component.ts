1881c6e6495f14d70c2d3a1fdf6ef9ebbafdf8cb
LautaroLeonel <lautarofer456@gmail.com>
2024-09-11 02:57:01 -0300

    primer commit - pasar todo el codigo de tailwind a css nativo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarElement } from './navbar/dto/navbar-element.type';
+import { LandingComponent } from './landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'giorgi-ford';
+  navbarElements: NavbarElement[] = 
+  [
+    { text: "Vehículos 0KM", href: "/" }, 
+    { text: "Vehículos usados", href: "#" }, 
+    { text: "Plan Ovalo", href: "#" },
+    { text: "Contáctanos", href: "#" }
+  ]
 }
